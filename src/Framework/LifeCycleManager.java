3873b4ff6045a679e934138e3364fb4c6ed1b5f0
sojungpp <sophie2458@naver.com>
2022-11-19 23:42:00 +0900

    Feat: 과목 조회

--- src/Framework/LifeCycleManager.java
+++ src/Framework/LifeCycleManager.java
@@ -3,6 +3,7 @@
  */
 package Framework;
 
+import Components.AddFilter.AddFilter;
 import Components.Middle.MiddleFilter;
 import Components.Sink.SinkFilter;
 import Components.Source.SourceFilter;
@@ -13,17 +14,21 @@ public class LifeCycleManager {
             CommonFilter filter1 = new SourceFilter("Students.txt");
             CommonFilter filter2 = new SinkFilter("Output.txt");
             CommonFilter filter3 = new MiddleFilter();
+            CommonFilter filter4 = new AddFilter();
             
             filter1.connectOutputTo(filter3);
-            filter3.connectOutputTo(filter2);
+            filter3.connectOutputTo(filter4);
+            filter4.connectOutputTo(filter2);
             
             Thread thread1 = new Thread(filter1);
             Thread thread2 = new Thread(filter2);
             Thread thread3 = new Thread(filter3);
+            Thread thread4 = new Thread(filter4);
             
             thread1.start();
             thread2.start();
             thread3.start();
+            thread4.start();
         } catch(Exception e) {
             e.printStackTrace();
         }
