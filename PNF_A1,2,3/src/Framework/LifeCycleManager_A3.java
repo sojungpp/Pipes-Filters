1bd4e73e5ee9c1d382d5a875131f83fd4dd26f3b
sojungpp <sophie2458@naver.com>
2022-11-26 21:32:51 +0900

    Chore: A1,2,3 합치기

---
+++ PNF_A1,2,3/src/Framework/LifeCycleManager_A3.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright(c) 2021 All rights reserved by Jungho Kim in Myungji University.
+ */
+package Framework;
+
+import java.util.Arrays;
+
+import Components.DeleteFilter.DeleteFilter;
+import Components.Id.IdFilter;
+import Components.Major.MajorFilter;
+import Components.Sink.SinkFilter;
+import Components.Source.SourceFilter;
+
+public class LifeCycleManager_A3 {
+    public static void main(String[] args) {
+        try {
+            CommonFilter filter1 = new SourceFilter("Students.txt");
+            CommonFilter filter2 = new SinkFilter("Output_A3.txt");
+            CommonFilter filter3 = new IdFilter("2013");
+            CommonFilter filter4 = new MajorFilter("EE");
+            CommonFilter filter5 = new DeleteFilter(Arrays.asList("17651", "17652"));
+            
+            filter1.connectOutputTo(filter3);
+            filter3.connectOutputTo(filter4);
+            filter4.connectOutputTo(filter5);
+            filter5.connectOutputTo(filter2);
+            
+            Thread thread1 = new Thread(filter1);
+            Thread thread2 = new Thread(filter2);
+            Thread thread3 = new Thread(filter3);
+            Thread thread4 = new Thread(filter4);
+            Thread thread5 = new Thread(filter5);
+            
+            thread1.start();
+            thread2.start();
+            thread3.start();
+            thread4.start();
+            thread5.start();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
